1aa8296e03978429edc354e38d7a0af3d203e549
Krishna Chandra Maurya <91846306+imkrishna-23@users.noreply.github.com>
2023-03-10 22:38:14 +0530

    Add files via upload

---
+++ array2d.java
@@ -0,0 +1,26 @@
+// import java.util.Scanner;
+
+// public class array2d {
+//     public static void main(String args[]) {
+//         System.out.println("Enter the row and coll");
+//         Scanner sc = new Scanner(System.in);
+//         int row = sc.nextInt();
+//         int col = sc.nextInt();
+//         int[][] array = new int[row][col];
+//         System.out.println("your array is :");
+//         // input row
+//         for (int i = 0; i < row; i++) {
+//             // input coll
+//             for (int j = 0; j < col; j++) {
+//                 array[i][j] = sc.nextInt();
+//             }
+//         }
+//         // output
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 System.out.print(array[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
